f9d66b9a56955a382cab5c249281980866db776b
Johan Durish <jdurish2@gmail.com>
2025-12-31 05:07:41 -0500

    Fix navigation

--- src/scripts/template.js
+++ src/scripts/template.js
@@ -1,6 +1,30 @@
 
-fetch("/src/templates/header.html")
-    .then(response => response.text())
-    .then(data => {
-        document.querySelector("header").innerHTML = data;
-    });
+const header = document.querySelector("header");
+
+const nav = document.createElement("nav");
+header.appendChild(nav);
+
+const fileNameOf = (doc) => doc.toLowerCase().replace(" ", "_");
+
+const docs = [
+    "Drawings",
+    "Digital Art",
+    "Miniatures",
+    "Smithing",
+    "About",
+    "Contact",
+];
+
+const homeAnchor = document.createElement("a");
+nav.appendChild(homeAnchor);
+
+homeAnchor.href = `/`;
+homeAnchor.innerText = "Home";
+
+for (const doc of docs) {
+    const anchor = document.createElement("a");
+    nav.appendChild(anchor);
+
+    anchor.href = `/${fileNameOf(doc)}`;
+    anchor.innerText = doc;
+}
